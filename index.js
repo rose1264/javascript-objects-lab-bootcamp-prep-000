6bd2b0ac6900f4b6b597663ebcfbfc84165cbf11
Shumei Huang <rose1264@gmail.com>
2017-10-17 14:17:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return object = Object.assign({},{key:value})
+  return Object.assign({},object,{key:value})
 }
